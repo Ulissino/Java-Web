629724a599c16a8140e9305cc2a8c356cb9d705c
Ivan Fazio <fazio.ivan20@gmail.com>
2023-11-08 15:21:05 +0100

    chunk 2

---
+++ Esercizio_giocattoli/Inventario.java
@@ -0,0 +1,55 @@
+package Esercizio_giocattoli;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Inventario {
+    
+    ArrayList<Giocattolo> g = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void aggiungiGiocattolo(){
+
+        //inseriamo i dati del giocattolo
+        System.out.println("Inserisci l'id del giocattolo");
+        int id = sc.nextInt();
+
+        sc.nextLine();
+        System.out.println("Inserisci il nome del giocattolo");
+        String nome = sc.nextLine();
+
+        System.out.println("Inserisci il prezzo del giocattolo");
+        double prezzo = sc.nextDouble();
+
+        System.out.println("Inserisci l'età consigliata");
+        int eta_consigliata = sc.nextInt();
+
+        //aggiungiamo il giocattolo all'interno dell'array
+        g.add(new Giocattolo(id, nome, prezzo, eta_consigliata));
+    }
+
+    public void rimuoviGiocattolo(){
+
+        System.out.println("Inserisci l'id del giocattolo da rimuovere");
+        int idrim = sc.nextInt();
+
+        //eseguo un ciclo for per scorrere l'array e trovare l'id da rimuovere
+        for (int i = 0; i < g.size(); i++) {
+            if (g.get(i).id == idrim) {
+                g.remove(i);
+                break; //faccio il break in modo tale che quando si trova l'id del giocattolo lo rimuove e non continua il programma 
+            }
+        }
+    }
+
+    public void visualizzaDati(){
+        for (int i = 0; i < g.size(); i++) {
+
+            System.out.println("Visualizzazione del prodotto");
+            System.out.println("ID giocattolo: "+ g.get(i).id);
+            System.out.println("Nome giocattolo: "+ g.get(i).nome);
+            System.out.println("Prezzo giocattolo: "+ g.get(i).prezzo);
+            System.out.println("Eta consigliata: "+ g.get(i).eta_cons);
+        }
+    }
+}
--- Esercizio_giocattoli/TestGiocat.java
+++ Esercizio_giocattoli/TestGiocat.java
@@ -1,15 +1,26 @@
 package Esercizio_giocattoli;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class TestGiocat{
+
+    final static String username = "Ivan20";
+    final static String pass = "12345";
+
     public static void main(String[] args) {
 
         //dichiariamo le nostre liste
         ArrayList <Vendita> v = new ArrayList<>(); 
         ArrayList <Giocattolo> g = new ArrayList<>(); 
-        ArrayList <Cliente> c = new ArrayList<>(); 
-        
+        ArrayList <Cliente> c = new ArrayList<>();
+        Inventario inventario = new Inventario();
+
+        Scanner sc = new Scanner(System.in);
+
+        /* 
         //aggiungiamo i giocattoli alla nostra lista
         g.add(new Giocattolo(0, "Asinello", 12.50, 10));
         g.add(new Giocattolo(1, "Moto", 5.50, 3));
@@ -22,5 +33,52 @@ public class TestGiocat{
         v.add(new Vendita(g.get(0), c.get(1)));
 
        System.out.println(v.toString());
+       */
+
+       System.out.println("Inserisci username: ");
+       String user = sc.nextLine();
+
+       System.out.println("Inserisci password: ");
+       String pas = sc.nextLine();
+
+       if (user.equals(username) && pas.equals(pass) ) {
+        System.out.println("------MENù------");
+        System.out.println("1) Aggiungi il giocattolo nell'inventario");
+        System.out.println("2) Rimuovi il giocattolo dell'inventario");
+        System.out.println("3) Visualizza il giocattolo nell'inventario");
+        System.out.println("4) Esci");
+        System.out.println("-------------------------------");
+        int risp = sc.nextInt();
+
+        switch (risp) {
+            case 1:
+                inventario.aggiungiGiocattolo();
+                System.out.println("---------------");
+                inventario.visualizzaDati();
+                break;
+
+            case 2:
+                inventario.visualizzaDati();
+                System.out.println("---------------");
+                inventario.rimuoviGiocattolo();
+                System.out.println("---------------");
+                inventario.visualizzaDati();
+                break;
+            
+            case 3:
+                inventario.visualizzaDati();
+                break;
+
+            case 4:
+                break;
+        }
+       }else{
+        System.out.println("-----SEI UN UTENTE-----");
+        inventario.visualizzaDati();
+       }
+
+
+
+
     }
 }
